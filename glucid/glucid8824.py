8606c9a065e299828be81209d955bbd2ef7fbcd1
Dan Mechanic <mechanic@columbia.edu>
2018-08-01 19:14:12 -0400

    quick update to RELEASES.txt

--- glucid/glucid8824.py
+++ glucid/glucid8824.py
@@ -23,7 +23,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.6.0a'
+__version__ = '0.2.0a'
 __author__ = 'Daniel R Mechanic (dan.mechanic@gmail.com)'
 CONFIGFILE='.glucid.cfg'
 
